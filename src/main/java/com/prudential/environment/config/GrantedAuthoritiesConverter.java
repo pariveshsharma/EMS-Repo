be968a03ce66d6f4b7b88471313a85cd24d2d540
parivesh sharma <parivesh.sharma9@gmail.com>
2025-04-07 13:40:39 +0530

    added render file

--- src/main/java/com/prudential/environment/config/GrantedAuthoritiesConverter.java
+++ src/main/java/com/prudential/environment/config/GrantedAuthoritiesConverter.java
@@ -1,34 +1,34 @@
-package com.prudential.environment.config;
-
-import com.nimbusds.jose.shaded.gson.internal.bind.JsonTreeReader;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-public class GrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
-    private static final String CLAIM_RESOURCE_ACCESS = "resource_access";
-    private static final String CLAIM_ROLES = "roles";
-    private static final String CLAIM_REALM_ACCESS = "realm_access";
-    public static final String PREFIX_RESOURCE_ACCESS = "ROLE_";
-    @Override
-    public Collection<GrantedAuthority> convert(Jwt source) {
-        Map<String, Object> resourceAccess = source.getClaimAsMap(CLAIM_RESOURCE_ACCESS);
-        Map<String, Object> resources = resourceAccess.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue()));
-        Map<String, Object> result = (Map<String, Object>) resources.get("account");
-        if(Objects.nonNull(resourceAccess)){
-            List<String> roles = (List<String>) result.get(CLAIM_ROLES);
-            if(Objects.nonNull(roles)){
-                return roles.stream().map(rm->new SimpleGrantedAuthority(PREFIX_RESOURCE_ACCESS+rm)).collect(Collectors.toList());
-            }
-        }
-
-        return List.of();
-    }
-}
+//package com.prudential.environment.config;
+//
+//import com.nimbusds.jose.shaded.gson.internal.bind.JsonTreeReader;
+//import org.springframework.core.convert.converter.Converter;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.oauth2.jwt.Jwt;
+//
+//import java.util.Collection;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Objects;
+//import java.util.stream.Collectors;
+//
+//public class GrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+//    private static final String CLAIM_RESOURCE_ACCESS = "resource_access";
+//    private static final String CLAIM_ROLES = "roles";
+//    private static final String CLAIM_REALM_ACCESS = "realm_access";
+//    public static final String PREFIX_RESOURCE_ACCESS = "ROLE_";
+//    @Override
+//    public Collection<GrantedAuthority> convert(Jwt source) {
+//        Map<String, Object> resourceAccess = source.getClaimAsMap(CLAIM_RESOURCE_ACCESS);
+//        Map<String, Object> resources = resourceAccess.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue()));
+//        Map<String, Object> result = (Map<String, Object>) resources.get("account");
+//        if(Objects.nonNull(resourceAccess)){
+//            List<String> roles = (List<String>) result.get(CLAIM_ROLES);
+//            if(Objects.nonNull(roles)){
+//                return roles.stream().map(rm->new SimpleGrantedAuthority(PREFIX_RESOURCE_ACCESS+rm)).collect(Collectors.toList());
+//            }
+//        }
+//
+//        return List.of();
+//    }
+//}
--- target/generated-sources/annotations/com/prudential/environment/mapper/BookingMapperImpl.java
+++ target/generated-sources/annotations/com/prudential/environment/mapper/BookingMapperImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-04T17:13:54+0530",
+    date = "2025-04-07T13:24:00+0530",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.14 (OpenLogic)"
 )
 public class BookingMapperImpl implements BookingMapper {
